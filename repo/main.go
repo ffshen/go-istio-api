ef171bf683f917dca74f2d8b0e306d5032f4fa17
shenxiang <shenxiang@mayihezi.com>
2019-05-21 08:57:04 +0800

    repo

---
+++ repo/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	log "github.com/sirupsen/logrus"
+	"net/http"
+)
+
+func main() {
+	http.Handle("/repo/v1/info", loggingMiddleware(http.HandlerFunc(handler)))
+	http.ListenAndServe(":9528", nil)
+}
+
+func handler(w http.ResponseWriter, req *http.Request) {
+	//fmt.Println(string(body))
+	fmt.Fprintf(w, "this is go istio repo : 9528 version : v1 ")
+}
+
+func loggingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		log.Infof("uri: %s", req.RequestURI)
+		next.ServeHTTP(w, req)
+	})
+}
